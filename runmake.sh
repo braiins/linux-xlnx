35e936ce71755ea7c61f1529922419219b057c48
PengPeng <516190948@qq.com>
2017-08-14 14:11:36 +0800

    将并行编译数目参数化,避免-j16卡死服务器

--- runmake.sh
+++ runmake.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+MAKE_JOBS=$2
+
 #git co inno
 #make distclean
 #make ARCH=arm xilinx_zynq_defconfig
 cp -rvf ./inno_files/config ./.config
-make -j16 ARCH=arm CROSS_COMPILE=arm-xilinx-linux-gnueabi- UIMAGE_LOADADDR=0x8000 uImage
+make -j${MAKE_JOBS} ARCH=arm CROSS_COMPILE=arm-xilinx-linux-gnueabi- UIMAGE_LOADADDR=0x8000 uImage
 #cp -rvf arch/arm/boot/uImage /home/Users/wangp/work/bin/ 
 
