2c9d3d4a4ce8cb76746c09c74b120e01a1911e9c
Libor Vašíček <libor.vasicek@braiins.cz>
2018-01-08 17:04:05 +0100

    Rewrite VID generator driver
    - Add checks for return values
    - Add vidgen to sysfs to the power directory
    - Remove vidgen from DTS because it is unnecessary

--- drivers/misc/vidgen.h
+++ drivers/misc/vidgen.h
@@ -1,24 +1,11 @@
-/*
-* blink.h - the header file with the ioctl definitions.
-*
-* The declarations here have to be in a header file, because
-* they need to be known both to the kernel module
-* (in chardev.c) and the process calling ioctl (ioctl.c)
-*/
 #ifndef VIDGEN_H
 #define VIDGEN_H
-#include <linux/ioctl.h>
-/*
-* The major device number. We can't rely on dynamic
-* registration any more, because ioctls need to know
-* it.
-*/
+
+#define DEVICE_NAME_0 "vidgen0.0"
+#define SYSDIR_NAME "vidgen"
+
 #define MAGIC_NUM 100
-/*
-* TURN ON LED ioctl
-*/
+
 #define IOCTL_SET_VALUE_0 _IOR(MAGIC_NUM, 0, char *)
 
-#define DEBUG
-#define DEVICE_FILE_NAME_0 "/dev/vidgen0.0"
 #endif
